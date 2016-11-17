7b7ae55bac816074e13a634fbad6caea9719d406
박민식 <totaldesigner@gmail.com>
2016-11-17 11:08:32 +0900

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -3,6 +3,14 @@ if [ -z "$HUB_PORT_4444_TCP_ADDR" ]; then
   exit 1
 fi
 
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
 phantomjs --webdriver=$ip:8080 --webdriver-selenium-grid-hub=http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT
+
+trap shutdown SIGTERM SIGINT
+wait $NODE_PID
